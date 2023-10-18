6aeebc0dc8b270c0e6f9f59dc134c44dcb4a6851
RubelHasan <rubelhasan306@gmail.com>
2023-10-18 10:15:08 +0100

    single post delete done

--- index.js
+++ index.js
@@ -65,6 +65,14 @@ async function run() {
       console.log(result);
     });
 
+    //delete service
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
